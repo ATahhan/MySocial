43e8dd5e52c782e0746d639e39aafaa623f378ba
Ammar AlTahhan <ammaraltahhan@Ammars-MacBook-Pro.local>
2017-06-04 17:36:53 +0300

    Completed Firebase integration

---
+++ MySocial/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MySocial
+//
+//  Created by Ammar AlTahhan on 6/4/17.
+//  Copyright © 2017 Ammar AlTahhan. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
