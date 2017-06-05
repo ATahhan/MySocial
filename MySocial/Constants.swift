6220153b86ee70db8a2a1815eace0d762ea6002b
Ammar AlTahhan <ammaraltahhan@Ammars-MacBook-Pro.local>
2017-06-05 03:04:11 +0300

    Custom sign in ui completed

---
+++ MySocial/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  MySocial
+//
+//  Created by Ammar AlTahhan on 6/5/17.
+//  Copyright © 2017 Ammar AlTahhan. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0/255.0
