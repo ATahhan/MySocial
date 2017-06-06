85546ba39406268326317c204b678e6649f83243
Ammar AlTahhan <ammaraltahhan@Ammars-MacBook-Pro.local>
2017-06-06 18:01:49 +0300

    Feed ui completed

--- MySocial/CircleView.swift
+++ MySocial/CircleView.swift
@@ -1,23 +1,26 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  MySocial
 //
-//  Created by Ammar AlTahhan on 6/5/17.
+//  Created by Ammar AlTahhan on 6/6/17.
 //  Copyright © 2017 Ammar AlTahhan. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
+class CircleView: UIImageView {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 3.0
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.cornerRadius = self.frame.width / 2
     }
 
 }
